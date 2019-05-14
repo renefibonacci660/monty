b7ae072435dc3a5170f1f06198eb3e22455636c7
Arthur Damm <indigoarthur@gmail.com>
2019-05-14 21:14:57 +0000

    Add initial stack module skeleton

--- monty.h
+++ monty.h
@@ -19,9 +19,29 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* stack.c */
+int push(stack_t **head, int n);
+int pop(stack_t **head);
+int peek(stack_t **head);
+int is_empty(stack_t **head);
 
 #endif
---
+++ stack.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+int push(stack_t **head, int n);
+
+int pop(stack_t **head);
+
+int peek(stack_t **head);
+
+int is_empty(stack_t **head);
