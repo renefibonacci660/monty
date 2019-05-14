0182222454ff2650a3b9a2c23b4fbe0fded004d0
renefibonacci660 <660@holbertonschool.com>
2019-05-14 19:24:15 +0000

    headerfile with protoype added

---
+++ monty.h
@@ -0,0 +1,27 @@
+#ifndef __MONTY__H
+#define __MONTY__H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+#endif
